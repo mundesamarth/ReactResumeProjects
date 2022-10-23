33c718fd6e7b21d8b49e4d3e1f33d90c22ba4dcc
samarth <munde.samarth28@gmail.com>
2022-10-23 23:52:03 +0530

    checking the master branch

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
-import Header from './Header.js'
-import Home from './Home.js'
+import "./App.css";
+import Header from "./Header.js";
+import Home from "./Home.js";
+import { BrowserRouter as Router, Switch,Route }
+from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Header/>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
