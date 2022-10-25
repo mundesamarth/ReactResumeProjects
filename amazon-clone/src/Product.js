daa092ae1639c9c3dd955d9920e3b8bbfdf74d5c
samarth <munde.samarth28@gmail.com>
2022-10-25 21:38:37 +0530

    dynamic adding of basket

--- amazon-clone/src/Product.js
+++ amazon-clone/src/Product.js
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id,title, price, rating, image }) {
+
+  const [{basket},dispatch] = useStateValue();
+
+
+  console.log('This is the basket' , basket)
+  const addToBasket = () =>{
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item:{
+        id:id,
+        title:title,
+        image:image,
+        price:price,
+        rating:rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product__info">
@@ -22,7 +40,7 @@ function Product({ id,title, price, rating, image }) {
         alt=""
       />
 
-      <button>Add To Basket</button>
+      <button onClick={addToBasket}>Add To Basket</button>
     </div>
   );
 }
