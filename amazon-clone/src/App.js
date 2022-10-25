fb81b930616c1618af95c077bf312665b8c5cb49
samarth <munde.samarth28@gmail.com>
2022-10-25 20:37:21 +0530

    checkout page started

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
 import Header from "./Header.js";
 import Home from "./Home.js";
+import Checkout from "./Checkout.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Header />
         <Switch>
+          <Route path="/checkout">
+            <Checkout />
+          </Route>
           <Route path="/">
-            <Header />
             <Home />
           </Route>
         </Switch>
